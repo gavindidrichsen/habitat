c463bcaa1fc9c37b6ea2b5700b3a94219dc625a1
Scott Macfarlane <smacfarlane@chef.io>
2019-10-28 09:43:36 -0700

    PATH insanity
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- .expeditor/scripts/verify/test_install_script.sh
+++ .expeditor/scripts/verify/test_install_script.sh
@@ -5,7 +5,7 @@ if ! command -v bats >/dev/null; then
   if [ "$(uname)" == "Darwin" ]; then
     brew install bats-core
   elif [ "$(uname)" == "Linux" ]; then
-    hab pkg install core/bats --binlink 
+    sudo hab pkg install core/bats --binlink 
   fi
 fi
 
@@ -14,10 +14,11 @@ if ! command -v gpg >/dev/null; then
   if [ "$(uname)" == "Darwin" ]; then
     brew install gnupg
   elif [ "$(uname)" == "Linux" ]; then
-    hab pkg install core/gnupg
-    hab pkg binlink core/gnupg gpg 
+    sudo hab pkg install core/gnupg
+    sudo hab pkg binlink core/gnupg gpg 
   fi
 fi
 
 echo "--- Testing install.sh"
+echo $PATH
 bats components/hab/tests/test_install_script.bats
