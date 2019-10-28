8ec939e2dc3c9713b86b452287447353d0c29039
Scott Macfarlane <smacfarlane@chef.io>
2019-10-28 10:10:11 -0700

    RAGE
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- .expeditor/scripts/verify/test_install_script.sh
+++ .expeditor/scripts/verify/test_install_script.sh
@@ -18,4 +18,6 @@ echo "--- Testing install.sh"
 # Bats in chefes/buildkite is a hab-binliked install to the default directory
 # of /bin, but /bin isn't on our path. 
 export PATH=$PATH:/bin
+echo $PATH 
+ls /bin/bats
 bats components/hab/tests/test_install_script.bats
