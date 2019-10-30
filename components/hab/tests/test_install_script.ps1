75f44e3076ca556c1d30e844888062f231c12cc9
Scott Macfarlane <smacfarlane@chef.io>
2019-10-30 07:08:42 -0700

    Check path of installed hab in test
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- components/hab/tests/test_install_script.ps1
+++ components/hab/tests/test_install_script.ps1
@@ -2,6 +2,7 @@ Describe "Install habitat using install.ps1" {
     It "can install the latest version of Habitat" {
         components/hab/install.ps1 -c dev
         $LASTEXITCODE | Should -Be 0
+        (Get-Command hab).Path | Should -Be "C:\ProgramData\Habitat\hab.exe"
     }
 
     It "can install a specific version of Habitat" {
